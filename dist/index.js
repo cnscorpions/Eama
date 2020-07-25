d1f91e82ddad0ad00eaf527cd9ca395c485ef0c7
Chan Gavin <justin.coding@gmail.com>
2020-07-25 17:36:52 +0800

    configure tsconfig.json

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
